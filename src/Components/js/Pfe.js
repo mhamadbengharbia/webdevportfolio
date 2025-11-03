f521422e861eb2d3851c8cc0a2fb6fca4c0c07d3
mhamed ben gharbia <mhamedbengharbia@gmail.com>
2025-11-03 14:46:42 +0100

    fix project section

--- src/Components/js/Pfe.js
+++ src/Components/js/Pfe.js
@@ -5,7 +5,7 @@ function Pfe() {
   
   
         return (
-          <div class="brand"  id="projects">
+          <div class="pro" >
             <h1>Print('CodingDojo Projects')
             </h1>
             <p className="pfe">Completed an intensive Software Development
@@ -13,74 +13,44 @@ function Pfe() {
               ability to build complete web applications and deepened my understanding of modern web technologies across multiple stacks.
 
 </p>
-          <div className="parts">
-                            <p  className="pfe"> <b>TFAREJ, HD Movie Streaming Platform (MERN) Avg: 8.25/10</b> </p>
+          <div className="firstproject">
+            <div>
+              <h3  > TFAREJ, HD Movie Streaming Platform (MERN) Avg: 8.25/10  </h3>
 
-              <div>
-              <ul>
-<li>Built a full-stack movie streaming platform featuring user authentication, movie search, and personalized watchlists. 
+               <p>
+ Built a full-stack movie streaming platform featuring user authentication, movie search, and personalized watchlists. 
   Implemented MongoDB for efficient management of user and movie data, and developed a responsive ReactJS frontend to ensure a smooth user experience. 
-  Added interactive features such as movie filtering, ratings, and comments to enhance engagement and usability.</li>
+  Added interactive features such as movie filtering, ratings, and comments to enhance engagement and usability.
+</p>
+            </div>
 
-  
-</ul>
+             <div>
+              <h3  > Biblio, Books Exchange Platform (C#/.Net) Avg: 9/10  </h3>
 
-
-
-              </div>
-              
-      <p  className="pfe">  <b>Biblio, Books Exchange Platform (C#/.Net) Avg: 9/10</b> </p>
-
-              <div>
-              <ul>
-<li>Developed a full-featured book exchange platform enabling users to list, search, and exchange books. Implemented secure user authentication with password hashing and session 
+               <p>Developed a full-featured book exchange platform enabling users to list, search, and exchange books. Implemented secure user authentication with password hashing and session 
   management to protect user data. Built a robust backend using C# and the .NET framework to handle book listings and user profiles, and integrated a SQL Server database to manage users,
    books, and exchange history. Designed a responsive and intuitive frontend with ASP.NET MVC,
-   featuring dynamic views, interactive forms, and advanced search and filter options to enhance user experience.</li>
+   featuring dynamic views, interactive forms, and advanced search and filter options to enhance user experience.
+</p>
+             </div>
 
-  
-</ul>
-
-
-
-              </div>
-
-
-
-                    <p  className="pfe"> <b> One Day Job, Job Search Platform (Python, Flask) Avg: 9/10</b></p>
 
               <div>
-              <ul>
-<li>Developed a web application for job seekers and employers, featuring secure user accounts, job postings, 
+                <h3  > One Day Job, Job Search Platform (Python, Flask) Avg: 9/10 </h3>
+
+               <p>
+Developed a web application for job seekers and employers, featuring secure user accounts, job postings, 
   and application management. Built the backend with Python (Flask) and implemented structured SQL database models for jobs, candidates, and companies. Designed a user-friendly frontend enabling seamless navigation between listings and applications,
-   and integrated advanced search and filter features by location, category, and skills to improve user experience.</li>
-
-  
-</ul>
-
-
-
+   and integrated advanced search and filter features by location, category, and skills to improve user experience.
+</p>
               </div>
-
-
+                            
+  
 
           </div>
           
  
 
-
-
-
-
-
-
-          
-          
-          
-          
-          
-          
-          
           
         
         </div>
